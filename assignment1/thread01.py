250f714efc35c36a25cdbda851a9fd3f4d2d0f71
sakaojai565956 <108925916+sakaojai565956@users.noreply.github.com>
2024-07-03 16:12:33 +0700

    asm1

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,10 +1,15 @@
 # running a function in another thread
+<<<<<<< HEAD
 from time import sleep, ctime 
+=======
+from time import sleep, ctime
+>>>>>>> da9298ed63f8e733278d4eb60fc55328115df595
 from threading import Thread
 
 # a custom function that blocks for a moment
 def task():
     # block for a moment
+<<<<<<< HEAD
     sleep (1)
     # display a message
     print(f'{ctime()} This is from another thread')
@@ -16,3 +21,16 @@ thread. start ()
 # wait for the thread to finish
 print(f'{ctime()}Waiting for yhe thread...')
 thread.join()
+=======
+    sleep(1)
+    # display a message
+    print(f'{ctime()} This is from another thread')
+
+# create a thread
+thread = Thread(target=task)
+# run the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
+>>>>>>> da9298ed63f8e733278d4eb60fc55328115df595
--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -3,6 +3,7 @@ from time import sleep, ctime
 from threading import Thread
 
 # a custom function that blocks for a moment
+<<<<<<< HEAD
 def task(sleep_time, message) :
     # block for a moment
     sleep (sleep_time)
@@ -11,6 +12,16 @@ def task(sleep_time, message) :
     
 # create a thread
 thread = Thread (target=task, args=(1.5, 'New message from another thread'))
+=======
+def task(sleep_time, message):
+    # block for a moment
+    sleep(sleep_time)
+    # display a message
+    print(f'{ctime()} {message}')
+
+# create a thread
+thread = Thread(target=task, args=(1.5, 'New message from another thread'))
+>>>>>>> da9298ed63f8e733278d4eb60fc55328115df595
 # run the thread
 thread.start()
 # wait for the thread to finish

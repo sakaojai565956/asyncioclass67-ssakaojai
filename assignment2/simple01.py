af9d0319cf5988c35958e5a955515f7446f809bc
sakaojai565956 <108925916+sakaojai565956@users.noreply.github.com>
2024-07-04 11:43:55 +0700

    02-03

--- assignment2/simple01.py
+++ assignment2/simple01.py
@@ -1,2 +1,21 @@
 # Synchronous cooking
 # 1 kitchen 1 chefs 1 dish
+
+from time import ctime, sleep, time
+
+#cooking synchronous
+def cooking(index):
+    print(f'{ctime()} Kitchen - {index} : Begin cooking...')
+    sleep(2)
+    print(f'{ctime()} Kitchen - {index} : Cooked done!')
+
+if __name__ == "__main__":
+    #begin of main thread
+    print(f'{ctime()} Main          : Start Cooking...')
+    start_time = time()
+
+    #cooking
+    cooking(0)
+
+    duration = time() - start_time
+    print(f'{ctime()} Main          : Finished Cooking duration in {duration:0.2f} seconds')
--- assignment3/asyncio04.py
+++ assignment3/asyncio04.py
@@ -1,1 +1,26 @@
 # Starting task
+
+from time import ctime
+import asyncio
+
+async def wash(basket):
+    print(f'{ctime()} : Washing Machine ({basket}): put the coin')
+    print(f'{ctime()} : Washing Machine ({basket}): start washing...')
+    await asyncio.sleep(5)
+    print(f'{ctime()} : Washing Machine ({basket}): Finished washing...')
+    return f'{ctime()}: ({basket}): is complete'
+
+async def main():
+    coro = wash('Basket A')
+    print(f'{ctime()}      : {coro}')
+    print(f'{ctime()}      : {type(coro)}')
+    #create task
+    task = asyncio.create_task(coro)
+    print(f'{ctime()}      : {task}')
+    print(f'{ctime()}      : {type(task)}')
+    #start task
+    result = await task
+    print(f'{ctime()}      : {result}')
+
+if __name__ == '__main__':
+    asyncio.run(main())

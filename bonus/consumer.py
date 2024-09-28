d6e41ba01f7eb9e8809ea3bff78d0cbfbc261ede
sakaojai565956 <108925916+sakaojai565956@users.noreply.github.com>
2024-09-28 14:04:18 +0700

    bonus

--- bonus/consumer.py
+++ bonus/consumer.py
@@ -1,1 +1,23 @@
 # https://aiokafka.readthedocs.io/en/stable/
+
+from aiokafka import AIOKafkaConsumer
+import asyncio
+
+async def consume():
+    consumer = AIOKafkaConsumer(
+        'my_topic',
+        bootstrap_servers=['192.168.43.194:9096', '192.168.43.113:9096', '192.168.43.11:9096', '192.168.43.147:9096'],  # เชื่อมต่อกับ Kafka brokers หลายตัว
+        # group_id="my-group"
+        )
+    # Get cluster layout and join group `my-group`
+    await consumer.start()
+    try:
+        # Consume messages
+        async for msg in consumer:
+            print("consumed: ", msg.topic, msg.partition, msg.offset,
+                  msg.key, msg.value, msg.timestamp)
+    finally:
+        # Will leave consumer group; perform autocommit if enabled.
+        await consumer.stop()
+
+asyncio.run(consume())
--- bonus/producer.py
+++ bonus/producer.py
@@ -1,1 +1,23 @@
 # https://aiokafka.readthedocs.io/en/stable/
+
+
+import asyncio
+from aiokafka import AIOKafkaProducer
+
+async def send_one():
+    # สร้าง instance ของ AIOKafkaProducer
+    producer = AIOKafkaProducer(
+        bootstrap_servers=['192.168.43.194:9096', '192.168.43.113:9096', '192.168.43.11:9096', '192.168.43.147:9096']  # เชื่อมต่อกับ Kafka brokers หลายตัว
+    )
+    # เริ่ม producer
+    await producer.start()
+    try:
+        # ส่งข้อความ
+        await producer.send_and_wait("my_topic", b"Hello, World!")
+        print("Message sent successfully!")
+    finally:
+        # ปิด producer
+        await producer.stop()
+
+# รันฟังก์ชัน send_one
+asyncio.run(send_one())
